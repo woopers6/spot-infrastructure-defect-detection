b789584cc92d6db71d6d8733f6abd2593512845b
Arun V <arunieboy@gmail.com>
2026-06-15 10:24:08 -0500

    add changes for it to build

---
+++ build/defect_detection/share/defect_detection/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/avaradar/ros2_ws/build/defect_detection"
--- src/defect_detection/setup.py
+++ src/defect_detection/setup.py
@@ -1,15 +1,27 @@
-from setuptools import find_packages, setup
+from glob import glob
+import os
+
+from setuptools import find_namespace_packages, setup
 
 package_name = 'defect_detection'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_namespace_packages(
+        include=[package_name, package_name + '.*'],
+        exclude=['test'],
+    ),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*')),
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*')),
+        (os.path.join('share', package_name, 'models'),
+            glob('models/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +36,12 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'image_publisher = defect_detection.spot_cam_loading.image_publisher:main',
+            'image_subscriber = defect_detection.spot_cam_loading.image_subscriber:main',
+            'point_cloud_subscriber = '
+            'defect_detection.pointcloud_loading.point_cloud_subscriber:main',
+            'yolo_detector = defect_detection.defect_detection.yolo_detector:main',
+            'fusion_node = defect_detection.defect_detection.fusion_node:main',
         ],
     },
 )
