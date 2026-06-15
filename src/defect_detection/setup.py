ed966c3934d6b892ce6199b323cd4899ad7af940
Arun V <arunieboy@gmail.com>
2026-06-15 10:50:24 -0500

    change maintainer stuff

--- src/defect_detection/setup.py
+++ src/defect_detection/setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='avaradar',
-    maintainer_email='avaradar@todo.todo',
+    maintainer_email='arunvaradarajan3@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
